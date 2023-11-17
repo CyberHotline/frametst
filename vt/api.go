924cb21829cd60a5f123e97008e0766fe4c4e7cd
Mohab Gabber <mohabgabber0@gmail.com>
2023-11-17 13:38:25 +0200

    Formatting, and expanded file behaviour

--- vt/api.go
+++ vt/api.go
@@ -16,65 +16,104 @@ type File struct {
 type FileInfo struct {
 	Data struct {
 		Attributes struct {
-			Name            []string `json:"names"`
-			Magic           string   `json:"magic"`
-			Sha256          string   `json:"sha256"`
-			Md5             string   `json:"md5"`
-			Sha1            string   `json:"sha1"`
-			CreationDate    int      `json:"creation_date"`
-			FirstSubmission int      `json:"first_submission_date"`
-			Size            int      `json:"size"`
-			Tags            []string `json:"tags"`
-			SignatureInfo   struct {
-				Product      string `json:"product"`
-				Description  string `json:"description"`
-				Copyright    string `json:"copyright"`
-				OriginalName string `json:"original name"`
-				FileVersion  string `json:"file version"`
-				InternalName string `json:"internal name"`
+			Name            []string `json:"names"`                 //D
+			Magic           string   `json:"magic"`                 //D
+			Sha256          string   `json:"sha256"`                //D
+			Md5             string   `json:"md5"`                   //D
+			Sha1            string   `json:"sha1"`                  //D
+			CreationDate    int      `json:"creation_date"`         //D
+			FirstSubmission int      `json:"first_submission_date"` //D
+			Size            int      `json:"size"`                  //D
+			Tags            []string `json:"tags"`                  //D
+			SignatureInfo   struct { //D
+				Product      string `json:"product"`       //D
+				Description  string `json:"description"`   //D
+				Copyright    string `json:"copyright"`     //D
+				OriginalName string `json:"original name"` //D
+				FileVersion  string `json:"file version"`  //D
+				InternalName string `json:"internal name"` //D
 			} `json:"signature_info"`
-			LastAnalysisStats struct {
-				Harmless         int `json:"harmless"`
-				TypeUnsupported  int `json:"type-unsupported"`
-				Suspicious       int `json:"suspicious"`
-				ConfirmedTimeout int `json:"confirmed-timeout"`
-				Timeout          int `json:"timeout"`
-				Failure          int `json:"failure"`
-				Malicious        int `json:"malicious"`
-				Undetected       int `json:"undetected"`
+			LastAnalysisStats struct { //D
+				Harmless         int `json:"harmless"`          //D
+				TypeUnsupported  int `json:"type-unsupported"`  //D
+				Suspicious       int `json:"suspicious"`        //D
+				ConfirmedTimeout int `json:"confirmed-timeout"` //D
+				Timeout          int `json:"timeout"`           //D
+				Failure          int `json:"failure"`           //D
+				Malicious        int `json:"malicious"`         //D
+				Undetected       int `json:"undetected"`        //D
 			}
-			Reputation int `json:"reputation"`
+			Reputation int `json:"reputation"` //D
 		} `json:"attributes"`
 	} `json:"data"`
 }
 
 type Filebehaviour struct {
 	Data struct {
-		CallsHighlighted    []string `json:"calls_highlighted"`
-		MutexesCreated      []string `json:"mutexes_created"`
-		MutexesOpened       []string `json:"mutexes_opened"`
-		ProcessesTerminated []string `json:"processes_terminated"`
-		ProcessTree         []struct {
-			Name string `json:"name"`
-			PID  string `json:"process_id"`
-		}
-		RegistryKeysOpened []string `json:"registry_keys_opened"`
-		RegistryKeysSet    []struct {
+		CallsHighlighted    []string   `json:"calls_highlighted"` //D
+		MutexesCreated      []string   `json:"mutexes_created"`
+		MutexesOpened       []string   `json:"mutexes_opened"`
+		ProcessesTerminated []string   `json:"processes_terminated"` //D
+		ProcessTree         []struct { //D
+			Name     string     `json:"name"`       //D
+			PID      string     `json:"process_id"` //D
+			Children []struct { //D
+				Name     string     `json:"name"`       //D
+				PID      string     `json:"process_id"` //D
+				Children []struct { //D
+					Name string `json:"name"`       //D
+					PID  string `json:"process_id"` //D
+				} `json:"children"`
+			} `json:"children"`
+		} `json:"processes_tree"`
+		RegistryKeysOpened  []string `json:"registry_keys_opened"`
+		RegistryKeysDeleted []string `json:"registry_keys_deleted"`
+		RegistryKeysSet     []struct {
 			Key   string `json:"key"`
 			Value string `json:"value"`
 		} `json:"registry_keys_set"`
-		MitreAttackTechniques []struct {
-			SignatureDescription string `json:"signature_description"`
-			ID                   string `json:"id"`
-			Severity             string `json:"severity"`
+		MitreAttackTechniques []struct { //D
+			SignatureDescription string `json:"signature_description"` //D
+			ID                   string `json:"id"`                    //D
 		} `json:"mitre_attack_techniques"`
-		Verdicts              []string `json:"verdicts"`
-		FilesOpened           []string `json:"files_opened"`
-		FileAttributesChanged []string `json:"files_attribute_changed"`
-		ModulesLoaded         []string `json:"modules_loaded"`
-		TextHighlighted       []string `json:"text_highlighted"`
-		MemoryPatternIps      []string `json:"memory_pattern_ips"`
-		MemoryPatternDomains  []string `json:"memory_pattern_domains"`
+		Verdicts              []string   `json:"verdicts"` //D
+		FilesOpened           []string   `json:"files_opened"`
+		FilesDeleted          []string   `json:"files_deleted"`
+		FilesWritten          []string   `json:"files_written"`
+		FileAttributesChanged []string   `json:"files_attribute_changed"`
+		ModulesLoaded         []string   `json:"modules_loaded"`
+		TextHighlighted       []string   `json:"text_highlighted"` //D
+		MemoryPatternIps      []string   `json:"memory_pattern_ips"`
+		MemoryPatternDomains  []string   `json:"memory_pattern_domains"`
+		HTTPConversations     []struct { //D
+			URL            string   `json:"url"`            //D
+			Method         string   `json:"request_method"` //D
+			ResonseHeaders struct { //D
+				ContentLength string `json:"Content-Length"` //D
+				SetCookie     string `json:"Set-Cookie"`     //D
+				StatusLine    string `json:"Status-Line"`    //D
+				Server        string `json:"Server"`         //D
+				Date          string `json:"Date"`           //D
+				ContentType   string `json:"Content-Type"`   //D
+			}
+		} `json:"http_conversations"`
+		IPTraffic []struct { //D
+			TransportLayerProtocol string `json:"transport_layer_protocol"` //D
+			DestinationIP          string `json:"destination_ip"`           //D
+			DestinationPort        int    `json:"destination_port"`         //D
+		} `json:"ip_traffic"`
+		DNSLookup []struct { //D
+			ResolvedIPs []string `json:"resolved_ips"` //D
+			Hostname    string   `json:"hostname"`     //D
+		} `json:"dns_lookups"`
+		CommandExecutions []string `json:"command_executions"` //D
+		MemoryDumps       []struct {
+			Process     string `json:"process"`
+			FileName    string `json:"file_name"`
+			Size        string `json:"size"`
+			BaseAddress string `json:"base_address"`
+			Stage       string `json:"stage"`
+		} `json:"memory_dumps"`
 	} `json:"data"`
 }
 
@@ -120,8 +159,3 @@ func Fretriever(key, id string, l int) {
 	file.Behaviour = b
 	FtableView(file, l)
 }
-
-// func Fileinfo(key, h *string, l *int) {
-// 	f := Fretriever(*key, *h)
-
-// }
